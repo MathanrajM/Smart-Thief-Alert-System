92ad98f967890bd561a294a0126370282bf65e8a
mathanrajhitech <116143705+mathanrajhitech@users.noreply.github.com>
2025-01-09 11:34:17 +0530

    Initial Commit

---
+++ utils/config.py
@@ -0,0 +1,6 @@
+# Configuration settings for the project
+MONGODB_URI = 'mongodb://localhost:27017/'
+DATABASE_NAME = 'SmartThiefAlertSystem'
+EMAIL_ADDRESS = 'mathanraj.mp2003@gmail.com'
+EMAIL_PASSWORD = 'Mathan.45@'
+IMAGE_PATH = 'C:/Users/matha/OneDrive/Desktop/Smart Thief Alert System/images/pic1.jpg'
